956a1898c8b0a46957dcaeddbc913a00e2bf6001
Umitosan <yeahdom@gmail.com>
2018-06-07 17:48:25 -0700

    add grid size adjust

--- app.js
+++ app.js
@@ -1,25 +1,31 @@
 /* jshint esversion: 6 */
 
+// let winHeight = $(window).height();
+// let winWidth = $(window).width();
+
+let winHeight = window.innerHeight;
+let winWidth = window.innerWidth;
 let grid = [];
-let winHeight = $(window).height();
-let winWidth = $(window).width();
-let boxSize = 20;
-let rowSize = Math.floor( (winWidth-20) / (boxSize) );
-let rowTotal = Math.floor( (winHeight-10) / (boxSize) );
+let boxSize = 50;
+let rowSize = Math.floor( (winWidth) / boxSize);
+let rowTotal = Math.floor( (winHeight-10) / boxSize);
 
 function Row(index, size, color) {
+  this.boxes = [];
   this.index = index;
+  this.size = size;
+  this.color = color;
+}
+
+function Box(index,size,color) {
+  this.index = index;
+  this.size = size;
   this.baseColor = color;
   this.color = color;
-  this.size = size;
 }
 
-function Box() {
 
-}
-
-function buildGrid() {
-
+function buildRows() {
   for (let i = 0; i < rowTotal; i++) {
     let c;
     if ( (i % 2) === 0 ) {
@@ -39,77 +45,81 @@ function buildGrid() {
     let ind = parseInt( $(this).attr('ind') );
     let r = grid[ind];
     $(this).css({
-      background: r.color,
-      height: r.size+'px',
+      height: boxSize,
+      width: boxSize*grid[0].length
     });
+  });
+}
+
+
+function buildBoxes() {
+  let tRow = [];
+  let c;
+
+  for (let r = 0; r < grid.length; r++) {
+
+    for (let j = 0; j < rowSize; j++) {
+      let boxColor;
+      if ( ((r+j) % 2) === 0 ) {
+        boxColor = 'lightblue';
+      } else {
+        boxColor = 'grey';
+      }
+      grid[r].boxes.push( new Box(j,boxSize,boxColor) );
+    }
+
+  }
+
+  $('.row').each(function(rIndex) {
+    for (var i = 0; i < rowSize; i++) {
+      let elem = " <div class='box' ind='"+i+"'></div> ";
+      $(this).append( elem );
+    }
+  });
+
+  $(".box").each(function(index) {
+      let rInd = parseInt( $(this).parent().attr('ind') );
+      let bInd = parseInt( $(this).attr('ind') );
+      let curBox = grid[rInd].boxes[bInd];
+      $(this).css({
+        "height": curBox.size+'px',
+        "width": curBox.size+'px',
+        "background-color": curBox.color,
+        float: 'left'
+      });
   });
 
   // events
-  $('.row').mouseenter(function(event1) {
-    // console.log('this = ',this);
+  $('.box').mouseenter(function(event1) {
     $(this).css("background","pink");
   }).mouseleave(function(event2) {
-    // console.log('$(this) = ',$(this));
-    let ind = parseInt( $(this).attr("ind") );
-    $(this).css("background",grid[ind].baseColor);
+    let rInd = parseInt( $(this).parent().attr('ind') );
+    let bInd = parseInt( $(this).attr('ind') );
+    $(this).css("background",grid[rInd].boxes[bInd].baseColor);
   });
 
-  // for (let j = 0; j < rowSize-1; j++) {
-  //   let tmpBox;
-  //
-  //   tmpRow.push(tmpBox)
-  // }
-  console.log('buildGrid run');
 }
 
 
 
 
-// function buildBars() {
-//
-//   for (let i = 0; i < 100; i++) {
-//     let c;
-//     if ( (i % 2) === 0 ) {
-//       c = 'lightblue';
-//     } else {
-//       c = 'grey';
-//     }
-//     bars.push( new Bar(i,barSize,c) );
-//   }
-//
-//   for (let i = 0; i < bars.length; i++) {
-//     let b = bars[i];
-//     let elem = " <div class='bar' ind='"+i+"'></div> ";
-//     $(".container-main").append( elem );
-//   }
-//
-//   $(".bar").each(function(index) {
-//     let ind = parseInt( $(this).attr('ind') );
-//     let b = bars[ind];
-//     $(this).css({
-//       background: b.color,
-//       height: b.size+'px',
-//     });
-//   });
-//
-//   // events
-//   $('.bar').mouseenter(function(event1) {
-//     // console.log('this = ',this);
-//     $(this).css("background","pink");
-//   }).mouseleave(function(event2) {
-//     // console.log('$(this) = ',$(this));
-//     let ind = parseInt( $(this).attr("ind") );
-//     $(this).css("background",bars[ind].baseColor);
-//   });
-//
-// }
 
 
+
+
+function buildGrid() {
+  buildRows();
+  buildBoxes();
+}
 
 ////////////////////////////////////
 ////////////////////////////////////
 $(document).ready( function() {
   console.log('page loaded');
+
+  $(".container-main").css("width", rowSize*boxSize); // prevents the rows from colapsing on window resize
+  $(".container-main").css("margin-left", ((winWidth-(boxSize*rowSize) )/2) ); // makes gaps same on both sides of the main container
+  $(".container-main").css("margin-top", ((winHeight-(boxSize*rowTotal) )/2) ); // makes gaps same on top and bottom
 
   buildGrid();
 
